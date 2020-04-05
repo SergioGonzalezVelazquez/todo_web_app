d84893dd6363ca3f55478285da25b6daf64c443b
Sergio Gonzalez Velazquez <Sergio.Gonzalez29@alu.uclm.es>
2020-04-05 18:15:49 +0200

    Added admin namespace.  Only admins can see the links to administration area.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+    
+    # If current_user is an admin, the method should run the code in 
+    # the block; if they’re not, it should show nothing. 
+    def admins_only(&block)
+        block.call if current_user.try(:admin?)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  
+  namespace :admin do
+    root "application#index"
+  end
+
   devise_for :users, :controllers => { registrations: 'registrations' }
 
   #Addtional tasks routes
