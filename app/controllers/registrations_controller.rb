54ec96e553506fb5ae312a9069ee622e0f8563bc
Sergio Gonzalez Velazquez <Sergio.Gonzalez29@alu.uclm.es>
2020-04-09 12:09:11 +0200

    Edit user password

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,16 +3,24 @@ class RegistrationsController < Devise::RegistrationsController
   # Allow users to edit their account without providing a password
   # https://github.com/heartcombo/devise/wiki/How-To:-Allow-users-to-edit-their-account-without-providing-a-password
   protected
+
   def update_resource(resource, params)
-    resource.update_without_password(account_update_params)
+    # if you submit a blank password, it should
+    # mean “don’t change the user’s current password.”
+    if params[:password].blank? && params[:password_confirmation].blank?
+      resource.update_without_password(account_update_params)
+    else
+      resource.update(account_update_params)
+    end
   end
 
   private
+
   def sign_up_params
     params.require(:user).permit(:first_name, :surname, :username, :email, :password, :password_confirmation)
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name, :surname, :username, :email)
+    params.require(:user).permit(:first_name, :surname, :username, :email, :password, :password_confirmation)
   end
 end
